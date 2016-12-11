d9b59a009fe901387de93ebe6f79bea6d7e48db0
oldirony <raffaele.abramini@gmail.com>
2016-12-11 19:42:24 +0000

    add sass support

--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -1,9 +1,26 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: [
-        './test/index.js'
+        './test/index.js',
+        './test/css/main.scss'
     ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract({
+                    loader: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
     devtool: 'source-map',
     output: {
         filename: './test/bundle.js'
-    }
+    },
+    plugins : [
+        new ExtractTextPlugin({ filename: './test/bundle.css', disable: false, allChunks: true })
+    ]
 };
